e974aab6938e9f0acb7f0c35b1ec32e44659cf0b
laurenbe06 <lauren_berning@icloud.com>
2024-09-19 08:34:24 -0400

    Create Book.java

---
+++ src/Book.java
@@ -0,0 +1,14 @@
+package geekText;
+
+@Entity
+public class Book {
+
+    private Long id;
+
+    private String title;
+    private String author;
+    private int yearPublished;
+    private String genre;
+    private String publisher;
+    private double ratings;
+}
