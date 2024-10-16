db78a31b8383e246d8766c6e0a95c26472e76b35
Ricardo Alvarez Dominguez <139882120+RicardoA12@users.noreply.github.com>
2024-10-15 20:41:14 -0400

    User.java

--- src/main/java/wishlist/User.java
+++ src/main/java/wishlist/User.java
@@ -1,19 +1,80 @@
 package wishlist;
 
-import lombok.Data;
+import book.ratings.project.model.Rating;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
 
 @Entity
-@Data
 
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
-    // Other user details (e.g., email, password)
+    private String email;    // Other user details (e.g., email, password)
+
+
+    // This defines a one-to-many relationship: a user can have multiple wishlists
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WishList> wishLists;
+
+    // This defines a one-to-many relationship: a user can have multiple ratings
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Rating> ratings;
+
+    // Constructors
+    public User() {}
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    // Getters and Setters for all fields
+    //Sarah could use this code
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<WishList> getWishLists() {
+        return wishLists;
+    }
+
+    public void setWishLists(List<WishList> wishLists) {
+        this.wishLists = wishLists;
+    }
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
 }
